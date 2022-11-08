8444c1d969991e1a1d09dc8a32ee3774b2cef615
Vijay Kumar <vijaynyaya@gmail.com>
2022-11-08 19:41:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const pianokeys = document.querySelectorAll('.key')
+
+function playSound(newUrl) {
+    console.log(newUrl)
+    new Audio(newUrl).play()
+}
+
+
+pianokeys.forEach((pianokey, i) => {
+    const number = i < 9 ? '0' + (i + 1) : (i + 1)
+    const newUrl = '24-piano-keys/key' + number + '.mp3'
+    console.log(newUrl)
+    pianokey.addEventListener('click', () => playSound(newUrl))
+})
